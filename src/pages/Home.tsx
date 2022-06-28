cf4738c73f46cec6fcf87c9d5d045085d12a4230
@yuri_assuncx <104099580+yuriassuncx@users.noreply.github.com>
2022-06-27 23:38:45 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Header } from "../components/Header";
 
 export function Home() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <div className="flex min-h-screen flex-col bg-spiderBackground bg-cover items-center justify-center py-2">
             <Header />
             <Cards />
         </div>
